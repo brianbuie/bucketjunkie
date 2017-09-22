9f66f48124f2f12a4488bb5f1bff5c784c829263
brianbuie <brianjamesbuie@gmail.com>
2017-09-21 23:01:23 -0500

    added eslint

--- handlers/errorHandlers.js
+++ handlers/errorHandlers.js
@@ -3,19 +3,18 @@
 
   With async/await, you need some way to catch errors
   Instead of using try{} catch(e) {} in each controller, we wrap the function in
-  catchErrors(), catch any errors they throw, and pass it along to our express middleware with next()
+  catchErrors(), catch any errors they throw,
+  and pass it along to our express middleware with next()
 */
 
-exports.catchErrors = (fn) => {
-  return function(req, res, next) {
-    return fn(req, res, next).catch(next);
-  };
+exports.catchErrors = fn => function (req, res, next) {
+  return fn(req, res, next).catch(next);
 };
 
 /*
   Not Found Error Handler
-
-  If we hit a route that is not found, we mark it as 404 and pass it along to the next error handler to display
+  If we hit a route that is not found, we mark it as 404 and pass it along
+  to the next error handler to display
 */
 exports.notFound = (req, res, next) => {
   const err = new Error('Not Found');
@@ -25,7 +24,6 @@ exports.notFound = (req, res, next) => {
 
 /*
   MongoDB Validation Error Handler
-
   Detect if there are mongodb validation errors that we can nicely show via flash messages
 */
 
@@ -40,15 +38,15 @@ exports.flashValidationErrors = (err, req, res, next) => {
 
 /*
   Development Error Hanlder
-
-  In development we show good error messages so if we hit a syntax error or any other previously un-handled error, we can show good info on what happened
+  In development we show good error messages so if we hit a syntax error
+  or any other previously un-handled error, we can show good info on what happened
 */
 exports.developmentErrors = (err, req, res, next) => {
   err.stack = err.stack || '';
   const errorDetails = {
     message: err.message,
     status: err.status,
-    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>')
+    stackHighlighted: err.stack.replace(/[a-z_-\d]+.js:\d+:\d+/gi, '<mark>$&</mark>'),
   };
   res.status(err.status || 500);
   res.format({
@@ -56,20 +54,19 @@ exports.developmentErrors = (err, req, res, next) => {
     'text/html': () => {
       res.render('error', errorDetails);
     }, // Form Submit, Reload the page
-    'application/json': () => res.json(errorDetails) // Ajax call, send JSON back
+    'application/json': () => res.json(errorDetails), // Ajax call, send JSON back
   });
 };
 
 
 /*
   Production Error Handler
-
   No stacktraces are leaked to user
 */
 exports.productionErrors = (err, req, res, next) => {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
-    error: {}
+    error: {},
   });
 };
--- helpers.js
+++ helpers.js
@@ -3,18 +3,19 @@
 */
 const fs = require('fs');
 exports.moment = require('moment');
-exports.dump = (obj) => JSON.stringify(obj, null, 2);
+
+exports.dump = obj => JSON.stringify(obj, null, 2);
 
 // inserting an SVG
-exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
+exports.icon = name => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = `NBA Fantasy`;
+exports.siteName = 'NBA Fantasy';
 
 exports.menu = [
-  { slug: '/stores', title: 'Stores', icon: 'store', },
-  { slug: '/tags', title: 'Tags', icon: 'tag', },
-  { slug: '/top', title: 'Top', icon: 'top', },
-  { slug: '/add', title: 'Add', icon: 'add', },
-  { slug: '/map', title: 'Map', icon: 'map', },
+  { slug: '/stores', title: 'Stores', icon: 'store' },
+  { slug: '/tags', title: 'Tags', icon: 'tag' },
+  { slug: '/top', title: 'Top', icon: 'top' },
+  { slug: '/add', title: 'Add', icon: 'add' },
+  { slug: '/map', title: 'Map', icon: 'map' },
 ];
