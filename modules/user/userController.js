df2955c5fa21b46c02811623e2cb437855ba6438
brianbuie <brianjamesbuie@gmail.com>
2017-09-24 22:02:00 -0500

    complete user system, mailer

--- modules/user/userController.js
+++ modules/user/userController.js
@@ -1,18 +1,117 @@
 const mongoose = require('mongoose');
-const User = mongoose.model('User');
 const promisify = require('es6-promisify');
 const passport = require('passport');
+const crypto = require('crypto');
+const mail = require('./mailHandler');
 
-exports.loginForm = (req, res) => {
-  res.render('account/login', { title: 'Login' });
+const User = mongoose.model('User');
+
+exports.account = (req, res) => res.render('account/account', { title: 'Account' });
+
+exports.createResetToken = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    req.flash('error', 'No user found with that e-mail address');
+    return res.redirect('/account/forgot-password');
+  }
+  user.resetPasswordToken = crypto.randomBytes(20).toString('hex');
+  user.resetPasswordExpires = Date.now() + 3600000;
+  await user.save();
+  const resetURL = `http://${req.headers.host}/account/reset-password/${user.resetPasswordToken}`;
+  mail.send({
+    user,
+    subject: 'Password Reset',
+    resetURL,
+    filename: 'password-reset',
+  });
+  req.flash('success', 'You have been emailed a password reset link.');
+  return res.redirect('/account/login');
 };
 
-exports.registerForm = (req, res) => {
-  res.render('account/register', { title: 'Register' });
+exports.forgotPasswordForm = (req, res) => res.render('account/forgot-password', { title: 'Forgot Password' });
+
+exports.isLoggedIn = (req, res, next) => {
+  if (req.isAuthenticated()) next();
+  req.flash('error', 'You must be logged in to do that.');
+  return res.redirect('/account/login');
 };
 
-exports.forgotPassword = (req, res) => {
-  res.render('account/forgot-password', { title: 'Forgot Password' });
+exports.login = passport.authenticate('local', {
+  failureRedirect: '/account/login',
+  failureFlash: 'Try again',
+  successRedirect: '/',
+  successFlash: 'Logged In',
+});
+
+exports.loginForm = (req, res) => res.render('account/login', { title: 'Login' });
+
+exports.logout = (req, res) => {
+  req.logout();
+  req.flash('success', 'logged out');
+  return res.redirect('/');
+};
+
+exports.register = async (req, res, next) => {
+  const user = new User({
+    email: req.body.email,
+    username: req.body.username,
+  });
+  const register = promisify(User.register, User);
+  await register(user, req.body.password);
+  next();
+};
+
+exports.registerForm = (req, res) => res.render('account/register', { title: 'Register' });
+
+exports.resetPasswordForm = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() },
+  });
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/account/forgot-password');
+  }
+  return res.render('account/reset-password', { title: 'Reset your password' });
+};
+
+exports.updateAccount = async (req, res) => {
+  const updates = {
+    username: req.body.username,
+    email: req.body.email,
+  };
+  await User.findOneAndUpdate(
+    { _id: req.user._id },
+    { $set: updates },
+    { new: true, runValidators: true, context: 'query' },
+  );
+  req.flash('success', 'Updated profile');
+  return res.redirect('back');
+};
+
+exports.updatePassword = async (req, res) => {
+  const user = await User.findOne({
+    resetPasswordToken: req.params.token,
+    resetPasswordExpires: { $gt: Date.now() },
+  });
+  if (!user) {
+    req.flash('error', 'Password reset is invalid or has expired');
+    return res.redirect('/account/forgot-password');
+  }
+  const setPassword = promisify(user.setPassword, user);
+  await setPassword(req.body.password);
+  user.resetPasswordToken = undefined;
+  user.resetPasswordExpires = undefined;
+  const updatedUser = await user.save();
+  await req.login(updatedUser);
+  req.flash('success', 'Your password has been reset');
+  return res.redirect('/');
+};
+
+exports.validatePasswordReset = (req, res, next) => {
+  if (req.body.password === req.body['confirm-password']) return next();
+  req.flash('error', 'Passwords do not match');
+  return res.redirect('back');
 };
 
 exports.validateRegister = (req, res, next) => {
@@ -30,44 +129,11 @@ exports.validateRegister = (req, res, next) => {
   const errors = req.validationErrors();
   if (errors) {
     req.flash('error', errors.map(err => err.msg));
-    res.render('account/register', {
+    return res.render('account/register', {
       title: 'Register',
       body: req.body,
       flashes: req.flash(),
     });
-    return;
   }
-  next();
+  return next();
 };
-
-exports.register = async (req, res, next) => {
-  const user = new User({
-    email: req.body.email,
-    username: req.body.username,
-  });
-  const register = promisify(User.register, User);
-  await register(user, req.body.password);
-  next();
-};
-
-exports.login = passport.authenticate('local', {
-  failureRedirect: '/account/login',
-  failureFlash: 'Try again',
-  successRedirect: '/',
-  successFlash: 'Logged In'
-});
-
-exports.logout = (req, res) => {
-  req.logout();
-  req.flash('success', 'logged out');
-  res.redirect('/');
-};
-
-exports.isLoggedIn = (req, res, next) => {
-  if(req.isAuthenticated()) {
-    next();
-    return;
-  }
-  req.flash('error', 'You must be logged in to do that.');
-  res.redirect('/account/login');
-}
--- server/routes.js
+++ server/routes.js
@@ -1,24 +1,25 @@
 const express = require('express');
+
 const router = express.Router();
 const userController = require('../modules/user/userController');
 const leagueController = require('../modules/league/leagueController');
 const { catchErrors } = require('../modules/error/errorHandlers');
 
 router.get('/', (req, res) => {
-  res.render('index');
+  res.render('index', { title: 'Home' });
 });
 
 // users
+router.get('/account', userController.isLoggedIn, userController.account);
+router.get('/account/forgot-password', userController.forgotPasswordForm);
+router.post('/account/forgot-password', catchErrors(userController.createResetToken));
 router.get('/account/login', userController.loginForm);
 router.post('/account/login', userController.login);
-router.get('/account/forgot-password', userController.forgotPassword);
-router.get('/account/register', userController.registerForm);
-router.post('/account/register',
-  userController.validateRegister,
-  userController.register,
-  userController.login
-);
 router.get('/account/logout', userController.logout);
+router.get('/account/register', userController.registerForm);
+router.post('/account/register', userController.validateRegister, userController.register, userController.login);
+router.get('/account/reset-password/:token', catchErrors(userController.resetPasswordForm));
+router.post('/account/reset-password/:token', userController.validatePasswordReset, catchErrors(userController.updatePassword));
 
 // leagues
 router.get('/leagues', userController.isLoggedIn, leagueController.leagues);
