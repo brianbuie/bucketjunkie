500a99fd6bf30ab4c3202a2a2ec55bb386f83873
brianbuie <brianjamesbuie@gmail.com>
2017-11-04 20:16:30 -0500

    acitivityFeed shell setup

--- client/src/components/Activity.js
+++ client/src/components/Activity.js
@@ -6,7 +6,8 @@ class Activity extends Component {
     super(props);
     this.state = { 
       collapseOpen: true,
-      activeCategory: "all"
+      activeCategory: "all",
+      showChatInput: true
     };
     this.categories = ["all", "chat", "rosters", "scores", "league"];
   }
@@ -16,14 +17,17 @@ class Activity extends Component {
   }
 
   changeCategory(category) {
-    this.setState({ activeCategory: category });
+    let newState = { activeCategory: category };
+    newState.showChatInput = ["all", "chat"].includes(category);
+    this.setState(newState);
   }
 
   render() {
     return (
       <div className="height-100 d-flex flex-column">
-        <Collapse isOpen={this.state.collapseOpen} className="flex-grow">
+        <Collapse isOpen={this.state.collapseOpen} className="flex-grow height-100">
           <div className="d-flex flex-column height-100">
+
             <Nav className="nav-fill">
               {this.categories.map(category => {
                 const className = this.state.activeCategory === category ? 'active' : '';
@@ -38,8 +42,17 @@ class Activity extends Component {
                 );
               })}
             </Nav>
-            <div id="activity__feed scroll-y pl-2 py-3 flex-grow height-100">
+
+            <div id="activity__feed" className="scroll-y pl-2 py-3 flex-grow height-100">
+              Activity
             </div>
+
+            <form className={this.state.showChatInput ? '' : 'hide'}>
+              <div className="form-group my-0 pr-0">
+                <input className="form-control" type="text" name="message" placeholder="Chat" autoComplete="off" />
+              </div>
+            </form>
+
           </div>
         </Collapse>
         <a href="" id="activity--collapse__toggler" onClick={() => this.toggleCollapse}>
