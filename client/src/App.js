4dd9014f820da1de6743488a014445d35598abeb
brianbuie <brianjamesbuie@gmail.com>
2018-01-06 12:27:54 -0600

    removed layout component

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import routes from 'routes';
-import Layout from 'components/Layout/Layout';
 import FetchManager from 'components/FetchManager/FetchManager';
 import Loading from 'components/Loading/Loading';
 import Toast from 'components/Toast/Toast';
+import Feed from 'components/Feed/Feed';
+import Nav from 'components/Nav/Nav';
+import AllPlayers from 'components/Pages/AllPlayers';
+import LeagueStandings from 'components/Pages/LeagueStandings';
+import OpenLeagues from 'components/Pages/OpenLeagues';
+import LeaguePage from 'components/Pages/LeaguePage';
+import CreateLeague from 'components/Pages/CreateLeague'; 
 
 const App = () => (
-  <div className="full-height">
+  <div className="full-height flex-row">
     <FetchManager />
     <Loading />
     <Toast />
-    <Layout />
+    <Nav />
+    <Route exact path="/teams" component={AllPlayers} />
+    <Route path="/teams/:team" component={AllPlayers} />
+    <Route path="/rosters" component={LeagueStandings} />
+    <Route path="/leagues/public" component={OpenLeagues} />
+    <Route path="/leagues/create" component={CreateLeague} />
+    <Route path="/league/:id" component={LeaguePage} />
+    <Feed />
   </div>
 );
 
--- client/src/components/Nav/Nav.js
+++ client/src/components/Nav/Nav.js
@@ -4,42 +4,47 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { withRouter } from 'react-router';
+import { Scrollbars } from 'react-custom-scrollbars';
 import { Nav as BootstrapNav, NavItem } from 'reactstrap';
 import TeamIcon from 'components/Team/TeamIcon';
 import routes from 'routes';
 
 const Nav = ({ teams, loc, league }) => (
-  <BootstrapNav vertical>
-    <li className="brand">
-      <img src="/images/logo-beta.svg" alt="BucketJunkie" />
-    </li>
-    {league && <NavItem active={loc.pathname === routes.rosters}>
-      <Link to={routes.rosters}>
-        <i className="fa fa-user-circle" />
-        Rosters
-      </Link>
-    </NavItem>}
-    <NavItem active={loc.pathname === routes.publicLeagues}>
-      <Link to={routes.publicLeagues}>
-        <i className="fa fa-server" />
-        Leagues
-      </Link>
-    </NavItem>
-    <NavItem active={loc.pathname === routes.teams}>
-      <Link to={routes.teams}>
-        <TeamIcon id='nba' />
-        Top Players
-      </Link>
-    </NavItem>
-    {teams.map(team => (
-      <NavItem active={loc.pathname === routes.teams + '/' + team._id} key={team._id}>
-        <Link to={`${routes.teams}/${team._id}`}>
-          <TeamIcon id={team._id} />
-          {team.abbreviation}
-        </Link>
-      </NavItem>
-    ))}
-  </BootstrapNav>
+  <div className="height-100 text-center" style={{ width: "200px" }}>
+    <Scrollbars autoHide>
+      <BootstrapNav vertical>
+        <li className="brand">
+          <img src="/images/logo-beta.svg" alt="BucketJunkie" />
+        </li>
+        {league && <NavItem active={loc.pathname === routes.rosters}>
+          <Link to={routes.rosters}>
+            <i className="fa fa-user-circle" />
+            Rosters
+          </Link>
+        </NavItem>}
+        <NavItem active={loc.pathname === routes.publicLeagues}>
+          <Link to={routes.publicLeagues}>
+            <i className="fa fa-server" />
+            Leagues
+          </Link>
+        </NavItem>
+        <NavItem active={loc.pathname === routes.teams}>
+          <Link to={routes.teams}>
+            <TeamIcon id='nba' />
+            Top Players
+          </Link>
+        </NavItem>
+        {teams.map(team => (
+          <NavItem active={loc.pathname === routes.teams + '/' + team._id} key={team._id}>
+            <Link to={`${routes.teams}/${team._id}`}>
+              <TeamIcon id={team._id} />
+              {team.abbreviation}
+            </Link>
+          </NavItem>
+        ))}
+      </BootstrapNav>
+    </Scrollbars>
+  </div>
 );
 
 const mapStateToProps = ({ teams, router, league }) => ({ teams, league, loc: router.location });
