e2a4eeb5005a4cfe9f065a9efca339d76cc05e14
brianbuie <brianjamesbuie@gmail.com>
2018-01-20 15:37:37 -0600

    prettier file upload

--- client/src/components/User/UserModal.js
+++ client/src/components/User/UserModal.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { Modal, ModalHeader, ModalBody, FormGroup, Button, Label } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, FormGroup, Button } from 'reactstrap';
 import A from 'components/Utilities/A';
 import UserPhoto from 'components/User/UserPhoto';
+import UserPhotoForm from 'components/User/UserPhotoForm';
 
 const UserModal = ({ username, photo, toggle, isOpen, submitNewPhoto, logout }) => {
   let form;
@@ -21,21 +22,19 @@ const UserModal = ({ username, photo, toggle, isOpen, submitNewPhoto, logout }) 
             Logout
           </A>
         </p>}
-        <div className="mx-auto p-3" style={{ width: '150px' }}>
-          <UserPhoto photo={photo} />
-        </div>
-        {submitNewPhoto && <form 
-          encType="multipart/form-data"
-          onSubmit={e => { e.preventDefault(); submitNewPhoto(new FormData(form)); toggle(); }}
-          ref={el => form = el}
-        >
-          <FormGroup className="text-center">
-            <input type="file" name="photo" accept="image/png, image/jpeg" />
-          </FormGroup>
-          <Button type="submit" color="primary" block outline>
-             Save →
-          </Button>
-        </form>}
+        {submitNewPhoto ? (
+          <UserPhotoForm 
+            photo={photo} 
+            className="mx-auto my-4" 
+            style={{ width: '200px' }} 
+            submit={data => { submitNewPhoto(data); toggle(); }}
+            cancel={toggle}
+          />
+        ) : (
+          <div className="mx-auto my-4" style={{ width: '200px' }}>
+            <UserPhoto photo={photo} />
+          </div>
+        )}
       </ModalBody>
     </Modal>
   );
