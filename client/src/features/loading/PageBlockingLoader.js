fe73bb184cf755336c9999011a9728ace26d5b38
brianbuie <brianjamesbuie@gmail.com>
2018-02-01 21:44:14 -0600

    BallLoaders

--- client/src/features/loading/PageBlockingLoader.js
+++ client/src/features/loading/PageBlockingLoader.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import './loadingStyle.scss';
+import { BouncingBallLoader } from 'features/loading/BallLoaders';
 
-const PageBlockingLoader = ({ globalLoading }) => (
-  globalLoading ? <div className="PageBlockingLoader"></div> : ''
-);
+const PageBlockingLoader = ({ globalLoading }) => globalLoading ? (
+  <div className="PageBlockingLoader">
+    <BouncingBallLoader />
+  </div>
+) : '';
 
 export default connect(
   ({ globalLoading }) => ({ globalLoading })
